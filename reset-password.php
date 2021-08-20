bad2bbb5ea3ded2c5515306417b6e44e70907a0d
NoSeimei <52064555+NoSeimei@users.noreply.github.com>
2021-08-20 15:57:15 +0400

    fix

--- reset-password.php
+++ reset-password.php
@@ -3,50 +3,38 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>Reset Password In PHP MySQL</title>
+<title>Réinitialisation du Mot de passe</title>
 <!-- CSS -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-<div class="container">
-<div class="card">
-<div class="card-header text-center">
-Reset Password In PHP MySQL
-</div>
-<div class="card-body">
 <?php
+include("Class/Users.php"); 
 if($_GET['key'] && $_GET['token'])
 {
-include "db.php";
 $email = $_GET['key'];
 $token = $_GET['token'];
-$query = mysqli_query($conn,
-"SELECT * FROM `users` WHERE `reset_link_token`='".$token."' and `email`='".$email."';"
-);
-$curDate = date("Y-m-d H:i:s");
-if (mysqli_num_rows($query) > 0) {
-$row= mysqli_fetch_array($query);
-if($row['exp_date'] >= $curDate){ ?>
-<form action="update-forget-password.php" method="post">
+$user = new Users();
+$theUser = $user->CheckResetPassword($email, $token);
+if ($theUser == true) { ?>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Asap" rel="stylesheet">
+    <link rel="stylesheet" href="css/login.css">
+
+<form class="login" action="update-forget-password.php" method="post">
 <input type="hidden" name="email" value="<?php echo $email;?>">
 <input type="hidden" name="reset_link_token" value="<?php echo $token;?>">
-<div class="form-group">
-<label for="exampleInputEmail1">Password</label>
-<input type="password" name='password' class="form-control">
-</div>                
-<div class="form-group">
-<label for="exampleInputEmail1">Confirm Password</label>
-<input type="password" name='cpassword' class="form-control">
-</div>
-<input type="submit" name="new-password" class="btn btn-primary">
+<h1>Zanimo</h1>
+        <input type="password" placeholder="Nouveau mot de passe" name="newpassword">
+        <input type="password" placeholder="Confirmation du mot de passe" name="confirmnewpassword">
+        <button>Connexion</button>
 </form>
-<?php } } else{
-echo "<p>This forget password link has been expired</p>";
-}
+<?php }  
 }
 ?>
-</div>
-</div>
-</div>
+
 </body>
 </html>
